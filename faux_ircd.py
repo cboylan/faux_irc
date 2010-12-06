9e024d2f5bf8d489555ec4b9cea91bc5f0f6ec3c
Clark Boylan <clark.boylan@gmail.com>
2010-12-06 13:09:03 -0800

    Very simple but working faux irc server. Needs more error checking.

--- faux_ircd.py
+++ faux_ircd.py
@@ -2,61 +2,120 @@
 
 import socket
 import select
+import re
 
-PORT=42425
+PORT=42427
 TIMEOUT=1.0
 EOM="\r\n"
 
 class IRCConnection(object):
     def __init__(self, sock, epoll, channels):
-        self._sock = sock
+        self.sock = sock
         self._fileno = sock.fileno()
-        self._epoll = epoll
-        self._channels = channels
+        self.epoll = epoll
+        self.channels = channels
         self.input_buf = ""
         self.output_buf = ""
+        self.nick = None
 
         self.handlers = {
             "NICK": IRCConnection.handle_nick,
             "JOIN": IRCConnection.handle_join,
+            "PART": IRCConnection.handle_part,
+            "LIST": IRCConnection.handle_list,
             "PRVMSG": IRCConnection.handle_prvmsg,
         }
 
     def fileno(self):
         return self._fileno
 
+    def write(self, message):
+        self.output_buf += message
+        self.epoll.modify(self._fileno, select.EPOLLOUT | select.EPOLLIN)
+
     def process_input(self):
-        self.input_buf += self._sock.recv(4096)
+        self.input_buf += self.sock.recv(4096)
         msgs = self.input_buf.rsplit(EOM, 1)
         if len(msgs) > 1:
             for msg in msgs[0].split(EOM):
-                self.output_buf += msg
-                if self.handlers(msg.split(" ", 1)[0], IRCConnection.default_handler)(self, msg):
-                    self._epoll.modify(self._fileno, select.EPOLLOUT | select.EPOLLIN)
+                if self.handlers.get(msg.split(" ", 1)[0], IRCConnection.default_handler)(self, msg):
+                    self.epoll.modify(self._fileno, select.EPOLLOUT | select.EPOLLIN)
             self.input_buf = msgs[1]
 
     def process_output(self):
-        bytes_written = self._sock.send(self.output_buf)
+        bytes_written = self.sock.send(self.output_buf)
         self.output_buf = self.output_buf[bytes_written:]
         if len(self.output_buf) == 0:
-            self._epoll.modify(self._fileno, select.EPOLLIN)
+            self.epoll.modify(self._fileno, select.EPOLLIN)
 
     def default_handler(self, msg):
         return False
 
     def handle_nick(self, msg):
-        args = msgs.split(" ")
-        if len(args) == 2:
-            self.nick = args[1]
+        regex = re.compile("^NICK (\w{1,9})$")
+        match = regex.match(msg)
+        if match:
+            self.nick = match.group(1)
             return False
         else:
+            self.output_buf += "ERROR 1" + EOM
             return True
 
     def handle_join(self, msg):
-        pass
+        if self.nick is not None:
+            regex = re.compile("^JOIN (#\w{0,199})$")
+            match = regex.match(msg)
+            if match:
+                l = self.channels.get(match.group(1), [])
+                l.append(self)
+                self.channels[match.group(1)] = l
+                return False
+            else:
+                self.output_buf += "ERROR 2" + EOM
+                return True
+        else:
+            return False
+
+    def handle_part(self, msg):
+        if self.nick is not None:
+            regex = re.compile("^PART (#\w{0,199})$")
+            match = regex.match(msg)
+            if match:
+                try:
+                    self.channels.get(match.group(1), []).remove(self)
+                    return False
+                except ValueError:
+                    self.output_buf += "ERROR 2" + EOM
+                    return True
+            else:
+                self.output_buf += "ERROR 2" + EOM
+                return True
+        else:
+            return False
+
+    def handle_list(self, msg):
+        if self.nick is not None:
+            regex = re.compile("^LIST$")
+            match = regex.match(msg)
+            if match:
+                for channel in self.channels.iterkeys():
+                    self.output_buf += "PRVMSG #status {c}".format(c=channel) + EOM
+                return True
+        return False
 
     def handle_prvmsg(self, msg):
-        pass
+        if self.nick is not None:
+            regex = re.compile("^PRVMSG (#\w{0,199}) ([\s\w]*)$")
+            match = regex.match(msg)
+            if match:
+                channel = self.channels.get(match.group(1), None)
+                if channel is not None:
+                    for conn in channel:
+                        conn.write("PRVMSG {c} {m}".format(c=match.group(1), m=match.group(2)) + EOM)
+                else:
+                    self.output_buf += "ERROR 2" + EOM
+                    return True
+        return False
 
 def process_loop():
     channels = {}
