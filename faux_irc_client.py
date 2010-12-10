f43d4298c7ff0e5aa6ced60371780049cf72a09b
Clark Boylan <clark.boylan@gmail.com>
2010-12-09 16:41:06 -0800

    Fix syntax errors and bugs in client skeleton.

--- faux_irc_client.py
+++ faux_irc_client.py
@@ -17,11 +17,12 @@ class IRCChannel(object):
     def setup(self):
         if not os.path.exists(self.directory):
             os.mkdir(self.directory)
-        self.infd = os.mkfifo(os.path.join(directory, "in"))
-        self.infile = os.fdopen(self.infd)
 
-        self.outfile = open(os.path.join(directory, "out"))
-        self.outfd = outfile.fileno()
+        if not os.path.exists(os.path.join(self.directory, "in")):
+            os.mkfifo(os.path.join(self.directory, "in"))
+        self.infd = os.open(os.path.join(self.directory, "in"), os.O_NONBLOCK | os.O_RDONLY)
+
+        self.outfd = os.open(os.path.join(self.directory, "out"), os.O_NONBLOCK | os.O_WRONLY | os.O_APPEND)
 
     def infileno(self):
         return self.infd
@@ -44,7 +45,6 @@ class ClientManager(object):
 
         self.sock = socket.socket(
             socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.setblocking(0)
 
         self.epoll = select.epoll()
 
@@ -58,7 +58,8 @@ class ClientManager(object):
         self.channels[self.channels["#status"].outfileno()] = self.channels["#status"]
 
         self.sock.connect((socket.gethostname(), self.port))
-        self.epoll.register(serversocket.fileno(), select.EPOLLIN)
+        self.sock.setblocking(0)
+        self.epoll.register(self.sock.fileno(), select.EPOLLIN)
         self.epoll.register(self.channels["#status"].infileno(), select.EPOLLIN)
 
     def process_msgs(self, msgs):
@@ -85,9 +86,9 @@ class ClientManager(object):
                     if len(msgs) > 1:
                         self.process_msgs(msgs[0])
                         ibuf = msgs[1]
-                else if event & select.EPOLLIN:
+                elif event & select.EPOLLIN:
                     # Read input pass to socket
-                    tempbuf = channels[fileno].process_input()
+                    tempbuf = self.channels[fileno].process_input()
                     if tempbuf is not None:
                         self.handle_client_input(tempbuf)
                         outbuf += tempbuf
@@ -98,9 +99,9 @@ class ClientManager(object):
                     outbuf = outbuf[bytes_written:]
                     if len(outbuf) == 0:
                         self.epoll.modify(fileno, select.EPOLLIN)
-                else if event & select.EPOLLOUT:
+                elif event & select.EPOLLOUT:
                     # Write out to channel file.
-                    if channels[fileno].process_output():
+                    if self.channels[fileno].process_output():
                         self.epoll.modify(fileno, select.EPOLLIN)
 
 if __name__ == "__main__":
