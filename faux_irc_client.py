7d00e40b50616ef1917c480da7cae91ad33a871e
Clark Boylan <clark.boylan@gmail.com>
2010-12-09 21:32:10 -0800

    Fix socket disconnects. Align to 80 characters. Add docstrings. Add comments.

--- faux_irc_client.py
+++ faux_irc_client.py
@@ -1,28 +1,45 @@
 #!/usr/bin/python2
+#
+# Clark Boylan
+# CS 494
+# Final Project
+# 12/10/2010
+# 
+# (Mostly) Non blocking Faux IRC client inspired by Suckless's ii IRC client.
+# Each channel creates and input FIFO and an output file through which
+# messages are entered and printed.
 
 import socket
-import select
 import re
 import os
 import sys
 from optparse import OptionParser
+import select
+if not "EPOLLRDHUP" in dir(select):
+        select.EPOLLRDHUP = 0x2000
 
 TIMEOUT=1.0
 EOM="\r\n"
 
+#TODO: Handle invalid joins and parts properly.
+
 class IRCChannel(object):
+    """Class used to manage the client side of an IRC channel."""
+
     def __init__(self, name, directory):
         self.name = name
         self.directory = os.path.join(directory, self.name)
         self.inbuf = ""
 
     def setup(self):
+        """Create an input FIFO and an output file for each channel."""
         if not os.path.exists(self.directory):
             os.mkdir(self.directory)
 
         if not os.path.exists(os.path.join(self.directory, "in")):
             os.mkfifo(os.path.join(self.directory, "in"))
-        self.infd = os.open(os.path.join(self.directory, "in"), os.O_NONBLOCK | os.O_RDONLY)
+        self.infd = os.open(os.path.join(self.directory, "in"),
+                os.O_NONBLOCK | os.O_RDONLY)
 
         self.outfile = open(os.path.join(self.directory, "out"), "w")
 
@@ -30,11 +47,13 @@ class IRCChannel(object):
         return self.infd
 
     def process_input(self):
+        """Read from the input FIFO and translate commands."""
         def translate_input(input_str):
+            """Translate user inputs into protocol compliant messages."""
             if input_str[0:5] == "/join":
                 return re.sub(r"/join", "JOIN", input_str)
             elif input_str[0:5] == "/part":
-                return re.sub(r"/part", "PART", input_str)
+                return re.sub(r"/part", "PART", input_str) + " " + self.name
             elif input_str[0:5] == "/nick":
                 return re.sub(r"/nick", "NICK", input_str)
             elif input_str[0:5] == "/list":
@@ -43,86 +62,103 @@ class IRCChannel(object):
                 return "PRVMSG " + self.name + " " + input_str
 
         self.inbuf += str(os.read(self.infd, 4096))
+        #Ignore any incomplete messages at the end of the read.
         msgs = self.inbuf.rsplit("\n", 1)
         if len(msgs) > 1:
             self.inbuf = msgs[1]
+            #Return the translated messages.
             return map(translate_input, msgs[0].split("\n"))
         return None
 
     def process_output(self, output):
+        """Write outputs to the output file."""
+        #TODO: Make this nonblocking.
         self.outfile.write(output)
         self.outfile.flush()
 
     def close(self):
+        """Close the file and FIFO associated with this channel."""
         os.close(self.infd)
         self.outfile.close()
         
 
 class ClientManager(object):
+    """Class used to manage an IRC connection and its associated channels."""
+
     def __init__(self, server, port, directory):
         self.channels = {}
         self.server = server
         self.port = port
         self.directory = directory
 
-        self.sock = socket.socket(
-            socket.AF_INET, socket.SOCK_STREAM)
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
         self.epoll = select.epoll()
 
     def setup(self):
+        """Setup the control directory, status channel, and communication
+        socket."""
         if not os.path.exists(self.directory):
             os.mkdir(self.directory)
 
         self.channels["#status"] = IRCChannel("#status", self.directory)
-        self.channels["#status"].setup()
-        self.channels[self.channels["#status"].infileno()] = self.channels["#status"]
+        status_channel = self.channels["#status"]
+        status_channel.setup()
+        self.channels[status_channel.infileno()] = status_channel
 
         self.sock.connect((socket.gethostname(), self.port))
         self.sock.setblocking(0)
-        self.epoll.register(self.sock.fileno(), select.EPOLLIN)
+        self.epoll.register(self.sock.fileno(),
+                select.EPOLLIN | select.EPOLLRDHUP)
         self.epoll.register(self.channels["#status"].infileno(), select.EPOLLIN)
 
     def close(self):
+        """Close all channels and the communication socket."""
         for key, channel in self.channels.items():
             if type(key) is str:
                 self.epoll.unregister(channel.infileno())
                 channel.close()
+
         self.epoll.unregister(self.sock.fileno())
         self.epoll.close()
         self.sock.close()
 
     def process_msgs(self, msgs):
+        """Deliver messages from the server to the appropriate channel."""
         err_regex = re.compile("^ERROR (\d)$")
         prvmsg_regex = re.compile("^PRVMSG (#\w{0,199}) (.*)$")
 
         for msg in msgs.split(EOM):
             err_match = err_regex.match(msg)
             prvmsg_match = prvmsg_regex.match(msg)
-            print msg
+
             if err_match and err_match.group(1) == "1":
-                print "Error 1"
                 self.channels["#status"].process_output("Invalid Nick\n")
             elif err_match and err_match.group(1) == "2":
-                print "Error 2"
-                self.channels["#status"].process_output("Invalid Join\n")
+                self.channels["#status"].process_output("Invalid Channel\n")
             elif prvmsg_match:
-                print "printing to output channel"
-                self.channels[prvmsg_match.group(1)].process_output(prvmsg_match.group(2) + "\n")
+                self.channels[prvmsg_match.group(1)].process_output(
+                        prvmsg_match.group(2) + "\n")
 
     def handle_client_input(self, fileno, msgs):
+        """Perform side effects that result from user input."""
         join_regex = re.compile("^JOIN (#\w{0,199})$")
-        part_regex = re.compile("^PART$")
+        part_regex = re.compile("^PART .*$")
+
         for msg in msgs:
             join_match = join_regex.match(msg)
             part_match = part_regex.match(msg)
+
             if join_match:
+                #Create a new channel as we have just joined to one.
                 name = join_match.group(1)
                 self.channels[name] = IRCChannel(name, self.directory)
-                self.channels[name].setup()
-                self.channels[self.channels[name].infileno()] = self.channels[name]
-                self.epoll.register(self.channels[name].infileno(), select.EPOLLIN)
+                new_channel = self.channels[name]
+                new_channel.setup()
+                self.channels[new_channel.infileno()] = new_channel
+                self.epoll.register(new_channel.infileno(), select.EPOLLIN)
             elif part_match:
+                #Remove the channel we just parted from.
                 name = self.channels[fileno].name
                 self.epoll.unregister(self.channels[name].infileno())
                 self.channels[name].close()
@@ -130,6 +166,7 @@ class ClientManager(object):
                 del self.channels[name]
 
     def loop(self):
+        """Main event loop. Loop over events and process them."""
         inbuf = ""
         outbuf = ""
 
@@ -137,7 +174,9 @@ class ClientManager(object):
             while True:
                 events = self.epoll.poll(TIMEOUT)
                 for fileno, event in events:
-                    if fileno == self.sock.fileno() and event & select.EPOLLHUP:
+                    if fileno == self.sock.fileno() and \
+                            event & select.EPOLLRDHUP:
+                        #Connection closed so we exit.
                         sys.exit()
                     if fileno == self.sock.fileno() and event & select.EPOLLIN:
                         # Read input pass to proper channel buffer.
@@ -152,26 +191,31 @@ class ClientManager(object):
                         if msgs is not None:
                             self.handle_client_input(fileno, msgs)
                             outbuf += EOM.join(msgs) + EOM
-                            self.epoll.modify(self.sock.fileno(), select.EPOLLOUT | select.EPOLLIN)
+                            self.epoll.modify(self.sock.fileno(),
+                                    select.EPOLLOUT | select.EPOLLIN |
+                                    select.EPOLLRDHUP)
                     if fileno == self.sock.fileno() and event & select.EPOLLOUT:
                         # Write out socket buffer
                         bytes_written = self.sock.send(outbuf)
                         outbuf = outbuf[bytes_written:]
                         if len(outbuf) == 0:
-                            self.epoll.modify(self.sock.fileno(), select.EPOLLIN)
+                            self.epoll.modify(self.sock.fileno(),
+                                    select.EPOLLIN | select.EPOLLRDHUP)
 
         finally:
+            #Cleanup in the event that the client exits.
             self.close()
             return
 
 if __name__ == "__main__":
     parser = OptionParser()
-    parser.add_option("-s", "--server",
-                      action="store", type="string", dest="server", default="localhost")
-    parser.add_option("-p", "--port",
-                      action="store", type="int", dest="port", default=42424)
-    parser.add_option("-d", "--dir",
-                      action="store", type="string", dest="directory", default=os.path.join(os.getcwd(), "faux_irc_channels"))
+    parser.add_option("-s", "--server", action="store", type="string",
+            dest="server", default="localhost")
+    parser.add_option("-p", "--port", action="store", type="int", dest="port",
+            default=42424)
+    parser.add_option("-d", "--dir", action="store", type="string",
+            dest="directory", default=os.path.join(os.getcwd(),
+            "faux_irc_channels"))
     (options, args) = parser.parse_args()
 
     manager = ClientManager(options.server, options.port, options.directory)
--- faux_ircd.py
+++ faux_ircd.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python2
 
 import socket
-import select
 import re
 from optparse import OptionParser
+import select
+if not "EPOLLRDHUP" in dir(select):
+        select.EPOLLRDHUP = 0x2000
 
 TIMEOUT=1.0
 EOM="\r\n"
 
-#TODO: Add NickManager, ChannelManager, and OptParse CLI option support
+#TODO: Add NickManager and ChannelManager.
 
 class IRCConnection(object):
+    """Class used to manage an IRC connection and its associated socket."""
     def __init__(self, sock, epoll, channels, nicks):
         self.sock = sock
         self._fileno = sock.fileno()
@@ -34,30 +37,54 @@ class IRCConnection(object):
         return self._fileno
 
     def write(self, message):
+        """Update the connections buffer and ask the poll notify us when we can
+        write to the socket"""
         self.output_buf += message
-        self.epoll.modify(self._fileno, select.EPOLLOUT | select.EPOLLIN)
+        self.epoll.modify(self._fileno,
+                select.EPOLLOUT | select.EPOLLIN | select.EPOLLRDHUP)
 
     def process_input(self):
+        """Read from socket then process the data that is read."""
         self.input_buf += self.sock.recv(4096)
+
+        #Separate complete messages from the possibly incomplete message at the
+        #end.
         msgs = self.input_buf.rsplit(EOM, 1)
+        #If at least one whole message is found process it.
         if len(msgs) > 1:
             for msg in msgs[0].split(EOM):
                 print(msg)
 
-                if self.handlers.get(msg.split(" ", 1)[0], IRCConnection.default_handler)(self, msg):
-                    self.epoll.modify(self._fileno, select.EPOLLOUT | select.EPOLLIN)
+                if self.handlers.get(msg.split(" ", 1)[0],
+                        IRCConnection.default_handler)(self, msg):
+                    self.epoll.modify(self._fileno,
+                            select.EPOLLOUT | select.EPOLLIN | select.EPOLLRDHUP)
             self.input_buf = msgs[1]
 
     def process_output(self):
+        """Write out this connections buffer to its socket."""
         bytes_written = self.sock.send(self.output_buf)
         self.output_buf = self.output_buf[bytes_written:]
         if len(self.output_buf) == 0:
-            self.epoll.modify(self._fileno, select.EPOLLIN)
+            self.epoll.modify(self._fileno, select.EPOLLIN | select.EPOLLRDHUP)
+
+    def close(self):
+        """Close a connection. Perform cleanup."""
+        self.sock.close()
+        if self.nick is not None:
+            self.nicks.remove(self.nick)
+        for channel in self.joined_channels:
+            self.channels[channel].remove(self)
+
+    #The following methods are message handlers. Each should return true if
+    #handling the message results in an output buffer with content. False
+    #otherwise.
 
     def default_handler(self, msg):
         return False
 
     def handle_nick(self, msg):
+        """Handle incoming NICK messages."""
         regex = re.compile("^NICK (\w{1,9})$")
         match = regex.match(msg)
         if match and match.group(1) not in self.nicks:
@@ -71,6 +98,7 @@ class IRCConnection(object):
             return True
 
     def handle_join(self, msg):
+        """Handle incoming JOIN messages."""
         if self.nick is not None:
             regex = re.compile("^JOIN (#\w{0,199})$")
             match = regex.match(msg)
@@ -87,9 +115,13 @@ class IRCConnection(object):
             return False
 
     def handle_part(self, msg):
+        """Handle incoming PART messages."""
         if self.nick is not None:
             regex = re.compile("^PART (#\w{0,199})$")
             match = regex.match(msg)
+
+            #If the nick is joined to the channel then remove them. Otherwise
+            #send an error to the client.
             if match and match.group(1) in self.joined_channels:
                 try:
                     self.channels.get(match.group(1), []).remove(self)
@@ -105,6 +137,7 @@ class IRCConnection(object):
             return False
 
     def handle_list(self, msg):
+        """Handle incoming LIST messages."""
         if self.nick is not None:
             regex = re.compile("^LIST$")
             match = regex.match(msg)
@@ -115,31 +148,31 @@ class IRCConnection(object):
         return False
 
     def handle_prvmsg(self, msg):
+        """Handle incoming PRVMSG messages."""
         if self.nick is not None:
             regex = re.compile("^PRVMSG (#\w{0,199}) (.*)$")
             match = regex.match(msg)
+
             if match:
+                #Mulitplex the message to all clients joined to the channel.
                 channel = self.channels.get(match.group(1), None)
                 if channel is not None and match.group(1) in self.joined_channels:
                     for conn in channel:
-                        conn.write("PRVMSG {c} {n}: {m}".format(c=match.group(1), n = self.nick, m=match.group(2)) + EOM)
+                        conn.write("PRVMSG {c} {n}: {m}".format(
+                                c=match.group(1), n = self.nick, m=match.group(2)) + 
+                                EOM)
                 else:
                     self.output_buf += "ERROR 2" + EOM
                     return True
         return False
 
-    def close(self):
-        self.sock.close()
-        if self.nick is not None:
-            self.nicks.remove(self.nick)
-        for channel in self.joined_channels:
-            self.channels[channel].remove(self)
-
 def process_loop(port):
+    """Main function and event loop processor for the server."""
     channels = {}
     connections = {}
     nicks = []
 
+    #Set up the listen socket.
     serversocket = socket.socket(
         socket.AF_INET, socket.SOCK_STREAM)
     serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -154,23 +187,30 @@ def process_loop(port):
         while True:
             events = epoll.poll(TIMEOUT)
             for fileno, event in events:
+                #If a new connection comes handle it.
                 if fileno == serversocket.fileno():
                     connection, address = serversocket.accept()
                     connection.setblocking(0)
                     irc_conn = IRCConnection(connection, epoll, channels, nicks)
-                    epoll.register(irc_conn.fileno(), select.EPOLLIN)
+                    epoll.register(irc_conn.fileno(),
+                            select.EPOLLIN | select.EPOLLRDHUP)
                     connections[irc_conn.fileno()] = irc_conn
                     continue
-                if event & select.EPOLLHUP:
+                #If a connection dies handle it.
+                if event & select.EPOLLRDHUP:
+                    print "EPOLL killing connection"
                     epoll.unregister(fileno)
                     connections[fileno].close()
                     del connections[fileno]
                     continue
+                #If a connection has input process it.
                 if event & select.EPOLLIN:
                     connections[fileno].process_input()
+                #If a connection has output process it.
                 if event & select.EPOLLOUT:
                     connections[fileno].process_output()
     finally:
+        #Cleanup when the server dies.
         for key, connection in connections.items():
             epoll.unregister(connection.fileno())
             connection.close()
@@ -181,8 +221,8 @@ def process_loop(port):
 
 if __name__ == "__main__":
     parser = OptionParser()
-    parser.add_option("-p", "--port",
-                      action="store", type="int", dest="port", default=42424)
+    parser.add_option("-p", "--port", action="store", type="int", dest="port",
+            default=42424)
     (options, args) = parser.parse_args()
 
     process_loop(options.port)
